276d520ab1016bdf98f331e81ed4d8c0d4ca8735
sashaij <o.zagorodniuk@outlook.com>
2023-10-06 16:41:29 +0300

    add algorythms to get radio buttons output; need fix the bug with final output
    BMR function itself do not give correct output

--- scripts.js
+++ scripts.js
@@ -1,42 +1,52 @@
 'use strict'
 
+const result = document.querySelector('.reuslt');
 const form = document.querySelector('form');
 const weight = document.getElementById('weight');
 const height = document.getElementById('height');
 const age = document.getElementById('age');
-const gender = document.querySelector('gender');
 const male = document.getElementById('male');
 const female = document.getElementById('female');
 const submitButton = document.getElementById('submit');
 const resetButton = document.getElementById('reset');
 
+let gender = '';
+let yourBMR;
+
+function genderValue() {
+    let ele = document.getElementsByName('gender');
+
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].checked){
+            gender = ele[i].value;
+        }
+    }
+}
 
 submitButton.addEventListener('click', () => {
-    displayValues();
+    genderValue();
+    BMR(gender);
+    result.textContent = yourBMR;
 })
 
-
-
-resetButton.addEventListener('click', () => {
-    weight.value = 0;
-    height.value = 0;
-    age.value = 0;
-})
 
 const displayValues = () => {
-    console.log('h: ' +  weight.value);
-    console.log('w: ' + height.value);
+    console.log('w: ' +  weight.value);
+    console.log('h: ' + height.value);
     console.log('a: ' + age.value);
-    console.log(gender.value);
+    
+    genderValue();
 }
 
 
 
 const BMR = function (isGender) {
     if (isGender === 'male') {
-        88.362 + (13.397 * weight.value) + (4.799 * height.value) - (5.677 * age.value);
+        yourBMR = 88.362 + (13.397 * weight.value) + (4.799 * height.value) - (5.677 * age.value);
+        console.log(yourBMR);
     } else if (isGender === 'female') {
-        447.593 + (9.247 * weight.value) + (3.098 * height.value) - (4.330 * age.value);
+        yourBMR = 447.593 + (9.247 * weight.value) + (3.098 * height.value) - (4.330 * age.value);
+        console.log(yourBMR);
     }
 }
 
