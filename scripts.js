91fb08a7083514683f7577b0ce924236fc4973ad
sashaij <o.zagorodniuk@outlook.com>
2023-10-07 11:30:20 +0300

    Add input restriction to prevent incorrect input from user

--- scripts.js
+++ scripts.js
@@ -14,6 +14,7 @@ let gender = '';
 let yourBMR = "";
 
 submitButton.addEventListener('click', () => {
+    inputRestrict();
     genderValue();
     BMR(gender);
 })
@@ -32,9 +33,32 @@ function genderValue() {
     }
 }
 
+const inputRestrict = function () {
+   if (weight.value > 250 || weight.value < 1) {
+        alert("Error. Input WEIGHT between 1 and 250");
+        weight.value = 0;
+        result.textContent = '0';
+        return; 
+    } else if (height.value > 300 || height.value < 1) {
+        alert("Error. Input HEIGHT between 1 and 300");
+        height.value = 0;
+        result.textContent = '0';
+        return;
+    } else if (age.value > 100 || age.value < 1) {
+        alert("Error. Input AGE beween 1 and 100");
+        age.value = 0;
+        result.textContent = '0';
+        return;
+    }
+}
 
 const BMR = function (isGender) {
-    if (isGender === 'male') {
+    if (weight.value > 250 || weight.value < 1 || 
+        height.value > 300 || height.value < 1 ||
+        age.value > 100 || age.value < 1) {
+        result.textContent = '0';
+        return;
+    } else if (isGender === 'male') {
         yourBMR = 88.362 + (13.397 * weight.value) + (4.799 * height.value) - (5.677 * age.value);
         result.textContent = yourBMR.toFixed(0);
     } else if (isGender === 'female') {
